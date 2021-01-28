c1a8c78809d9b067bb4ed9a761522d6cb129713d
rusgar <edamamon@gmail.com>
2021-01-28 13:11:36 +0100

    Cambio de conexion del servidor

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = 'mongodb+srv://new-user_01:user01@cluster0.eml2i.mongodb.net/sample_mflix?retryWrites=true&w=majority';
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI,{
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
