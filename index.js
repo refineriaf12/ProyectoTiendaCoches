e41b18a68d227bd212c09f2c8c5cd8d7cbe94971
rusgar <edamamon@gmail.com>
2021-02-02 12:35:59 +0100

    Enrutado Pagina Principal
    Diseño atomico de cada cosa

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-'use strict'
-const port = 8000;
-const morgan = require('morgan')
-const handlebars = require('express-handlebars');
-const express= require('express');
-const app = express();
-
-require('./database');
-
-app.engine(
-    "hbs",
-    handlebars({
-      layoutsDir: __dirname + "/views/layouts",
-      extname: "hbs",
-      partialsDir: __dirname + "/views/partials",
-    })
-  );
-
-  app.use(express.static("public"));
-
-  app.listen(port, () => console.log(`Leyendo el puerto ${port}`));
-
-
-
+  'use strict'
+  const express= require('express');
+  const app = express();
+  const handlebars = require('express-handlebars');
+  const morgan = require('morgan')
+  const port = 8000;
+  
+  require('./database');
+  
+  app.engine(
+      "hbs",
+      handlebars({
+        layoutsDir: __dirname + "/views/layouts",
+        partialsDir: __dirname + "/views/partials",
+        extname: "hbs"
+      })
+    );
+  
+    app.set('view engine', 'hbs');
+  
+    app.get("/", (req, res) => {
+      res.render("templates/home", {layout: "main"});
+    });
+  
+  
+    app.use(express.static("public"));
+    app.use(express.urlencoded({extended:false}));
+    app.use(express.json());
+    
+  
+  
+    app.listen(port, () => console.log(`Leyendo el puerto ${port}`));
 
 
