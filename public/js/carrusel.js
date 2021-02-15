77eb5ad3ee6d004e3300a63915f8e02d4c1792ed
Julian <diazg.julian@gmail.com>
2021-02-15 14:53:01 +0100

    Carrusel portada
    Edición lista elementos y clases, creación js funcionalidad, maquetación elementos

--- public/js/carrusel.js
+++ public/js/carrusel.js
@@ -1,40 +1,108 @@
 'use strict'
-const slider = document.querySelector(".items");
-		const slides = document.querySelectorAll(".item");
-		const button = document.querySelectorAll(".button");
 
-		let current = 0;
-		let prev = 4;
-		let next = 1;
+window.onload = function addClass(){
 
-		for (let i = 0; i < button.length; i++) {
-			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-		}
+  var carousel = document.querySelector('.O-slider__slides');
+	var seats = [...document.querySelectorAll(".M-slider__listItem")];
 
-		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+	seats.slice(-1)[0].classList.add("-is-ref");
 
-		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+  document.addEventListener("click", delegate(toggleFilter, toggleHandler));
 
-		const gotoNum = number => {
-			current = number;
-			prev = current - 1;
-			next = current + 1;
+// Common helper for event delegation.
+  function delegate(criteria, listener) {
 
-			for (let i = 0; i < slides.length; i++) {
-				slides[i].classList.remove("active");
-				slides[i].classList.remove("prev");
-				slides[i].classList.remove("next");
-			}
+    return function(e) {
 
-			if (next == 5) {
-				next = 0;
-			}
+      var el = e.target;
+      do {
 
-			if (prev == -1) {
-				prev = 4;
-			}
+        if (!criteria(el)) {
+         continue;
+        }
 
-			slides[current].classList.add("active");
-			slides[prev].classList.add("prev");
-			slides[next].classList.add("next");
-		}
+        e.delegateTarget = el;
+        listener.call(this, e);
+        return;
+
+      }while ((el = el.parentNode));
+
+  };
+
+}
+
+// Custom filter to check for required DOM elements
+  function toggleFilter(elem) {
+
+    return (elem instanceof HTMLElement) && elem.matches(".M-slider__button");
+    // OR
+    // For < IE9
+    // return elem.classList && elem.classList.contains('btn');
+  }
+
+  // Custom event handler function
+  function toggleHandler(e) {
+
+    var newSeat;
+    var el = document.querySelector('.-is-ref');
+    var currSliderControl = e.delegateTarget;
+    // Info: e.target is what triggers the event dispatcher to trigger and e.currentTarget is what you assigned your listener to.
+
+    el.classList.remove('-is-ref');
+    if (currSliderControl.getAttribute('data-toggle') === 'next') {
+
+      newSeat = next(el);
+      carousel.classList.remove('-is-reversing');
+
+    }else {
+
+      newSeat = prev(el);
+      carousel.classList.add('-is-reversing');
+
+    }
+
+    newSeat.classList.add('-is-ref');
+    newSeat.style.order = 1;
+
+    for (var i = 2; i <= seats.length; i++) {
+
+      newSeat = next(newSeat);
+      newSeat.style.order = i;
+
+    }
+
+    carousel.classList.remove('-is-set');
+    return setTimeout(function() {
+
+      return carousel.classList.add('-is-set');
+
+    }, 50);
+
+    function next(el) {
+
+      if (el.nextElementSibling) {
+
+        return el.nextElementSibling;
+
+      }else {
+
+        return carousel.firstElementChild;
+
+      }
+    
+    }
+
+    function prev(el) {
+
+      if (el.previousElementSibling) {
+
+        return el.previousElementSibling;
+
+      } else {
+
+        return carousel.lastElementChild;
+
+      }	
+    }
+  }
+};
