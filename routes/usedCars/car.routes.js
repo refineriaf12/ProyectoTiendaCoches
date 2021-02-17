73e4aeee31ec21db5b31a8df2930331803b494a2
Julian <diazg.julian@gmail.com>
2021-02-17 13:18:03 +0100

    añadir controllers

--- routes/usedCars/car.routes.js
+++ routes/usedCars/car.routes.js
@@ -2,7 +2,7 @@
 
 const { Router } = require ('express');
 const router = Router();
-const carController = require('../controllers/car.controller');
+const carController = require('../../controllers/usedCars/car.controller');
 
 
 
