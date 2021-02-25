7aee780f5dd2161c7f5bd9eca522afe2b1df56a3
Angel <angelmanuelmartinezfernandez@gmail.com>
2021-02-25 14:31:08 +0100

    Añadido archivo testconfig y usedCars.test.js

---
+++ test/secondhand/usedCars.test.js
@@ -0,0 +1,27 @@
+// TESTING DE CONEXIÓN BDD
+
+// requerimos la bdd y el modelo
+const mongoose = require('mongoose');
+const UsedCars = require('../../models/usedCars')
+
+// creamos datos según el modelo para testear
+const carData = { carBrand: 'Toyota', carModel: 'Yaris', nextItvDate: new Date(), sellingPrice: 'Number', carImage: 'URL image' , carColor: 'red' , seatsNumber: 5 , doorNumber: 4 , transmissionType: "automática", motorType: "diesel"};
+
+
+
+describe('Model Test', () => {
+
+    describe('Second Used Cars Model Test', () => {
+
+        it('Create and save users successfully', async () => {
+            const validCar = new UsedCars(carData);         // creamos el objeto
+            const savedUsedCar = await validCar.save();     // lo guardamos en la bdd
+
+            expect(savedUsedCar._id).toBeDefined();
+
+        })
+
+    });
+
+
+});
