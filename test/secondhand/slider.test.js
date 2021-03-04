992e0bb014ed7bad5240b6be5c622a1f419c6c03
Angel <angelmanuelmartinezfernandez@gmail.com>
2021-03-04 15:29:06 +0100

    Solucionados y añadidos test unitarios slider

--- test/secondhand/slider.test.js
+++ test/secondhand/slider.test.js
@@ -6,7 +6,7 @@ const { findAllCars ,carsLength, generarRamdon, generoArray3Ramdon, arrayAd } = 
 const UsedCarModel = require('../../models/usedCars');
 
 const carData ={ carBrand: 'Toyota', carModel: 'Yaris', modelYear: 122};
-
+const regexRandom = /^[0-2]?/;
 
 let  mongo;
 
@@ -20,7 +20,7 @@ let savedUsedCar;
 
 describe('Slider functions test', () =>{
 
-    it('Crear y guardar coche correctamente', async () => {
+    it('Creamos 3 coches en la base de datos simulada en memoria, para testear funcionalidad', async () => {
         for(let i=0; i<3; i++){
             let validCar = new UsedCarModel(carData);
             savedUsedCar = await validCar.save();
@@ -29,29 +29,35 @@ describe('Slider functions test', () =>{
 
     it('Recuperamos elementos de la base de datos', async () =>{
         const allCars = await findAllCars();
+        expect(allCars).toBeDefined();
         expect(allCars.length).toBe(3);
     });
 
     it('La función carLength debe devolver 3', async () =>{
-        console.log( await UsedCarModel.find().lean());
         expect(await carsLength()).toBe(3);
     });
 
-    it.skip('la función generar random debe devolver un número entre 0 y 3', async () =>{
-        expect(await generarRamdon()).toBe("^[0-3]?");
+    it('la función generar random debe devolver un número entre 0 y 2', async () =>{
+        expect((await generarRamdon()).toString()).toMatch(/[0-2]/);
     });
 
-    it.skip('la función generoArray3Ramdon debe devolver un array con tres números entre 0 y 3', async  () =>{
-        expect(await generoArray3Ramdon()).toBe(["^[0-3]"])
+    it('la función generoArray3Ramdon debe devolver un array con tres números entre 0 y 3', async  () =>{
+        expect((await generoArray3Ramdon())[0]).not.toBeNaN();
+        expect((await generoArray3Ramdon())[1]).not.toBeNaN();
+        expect((await generoArray3Ramdon())[2]).not.toBeNaN();
+
+        expect((await generoArray3Ramdon()).length).toBe(3);
+        expect(((await generoArray3Ramdon())[0]).toString()).toMatch(/[0-2]/);
+        expect(((await generoArray3Ramdon())[1]).toString()).toMatch(/[0-2]/);
+        expect(((await generoArray3Ramdon())[2]).toString()).toMatch(/[0-2]/);
+
+
     });
 
     it.skip('la función arrayAd debe devolver un array con tres objetos coche', async () =>{
-        expect(await  arrayAd()).toBe([savedUsedCar]);
+        expect(await  arrayAd()).toBe([/^[0-2]?/]);
     })
 })
-
-
-
 
 afterAll(async () => {
 
