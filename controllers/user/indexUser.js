8391f0c12c13bfcf999364c91e93b285e80a4f1f
rusgar <edamamon@gmail.com>
2021-03-05 12:20:11 +0100

    mas pruebas, pasa una

--- controllers/user/indexUser.js
+++ controllers/user/indexUser.js
@@ -1,7 +1,7 @@
 'use strict';
-
- const createNewUser=  async (request,response) => {
-    const {name,surname,dniNumber,userName,email,password} = request.body;
+const User = require('../../models/user');
+ exports.createNewUser=  async (request,response) => {
+    const {name,surname,dniNumber,userName,email,password} = {name: "a",surname:"b",dniNumber:"c",userName:"d",email: "e", password: "f"};
     const newUser = new User ({name,surname,dniNumber,userName,email,password});
     await newUser.save();
     response.redirect("/login");
@@ -20,4 +20,3 @@
     
     };
 
-  module.exports =(createNewUser)
